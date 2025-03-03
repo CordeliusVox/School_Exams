e9df0e66b52b878d1c1af71917897ceca0bd8942
CordeliusVox <cordeliusvox@gmail.com>
2025-03-03 12:48:53 +0200

    Update Question_1.cs

--- March 3th 2025/Question_1.cs
+++ March 3th 2025/Question_1.cs
@@ -1,1 +1,76 @@
-ha
+// א. פעולה שמחשבת את ההנחה למשפחה אחת
+public static int CalculateDiscount(int People, int Students)
+{
+    int discount1 = 0;
+    int discount2 = Students * 40;
+
+    if (People > 6)
+    {
+        discount1 = 100;
+    }
+
+    if (discount1 > discount2)
+    {
+        return discount1;
+    }
+    else
+    {
+        return discount2;
+    }
+}
+
+// ב. פעולה שמחשבת את סך כל ההנחות ביישוב מסוים
+public static int CalculateTotalDiscount(int Families)
+{
+    int Total = 0;
+
+    for (int i = 0; i < Families; i++)
+    {
+        Console.Write("enter amount of people");
+        int People = int.Parse(Console.ReadLine());
+
+        Console.Write("enter amount of students");
+        int Students = int.Parse(Console.ReadLine());
+
+        Total = Total + CalculateDiscount(People, Students);
+    }
+
+    return Total;
+}
+
+// ג. פעולה שבודקת אילו יישובים חרגו מהתקציב של המועצה
+public static void CheckBudget(int[] rdc)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write($"כמה משפחות יש ביישוב {i}? ");
+        int numOfFamilies = int.Parse(Console.ReadLine());
+
+        int totalDiscount = CalculateTotalDiscountForTown(numOfFamilies);
+
+        if (totalDiscount > rdc[i])
+        {
+            Console.WriteLine($"היישוב {i} חרג מהתקציב! קיבל {totalDiscount} ש\"ח כאשר התקציב היה {rdc[i]} ש\"ח.");
+        }
+    }
+}
+
+// פונקציה ראשית שמפעילה את התוכנית
+public static void Main()
+{
+    // מערך שנתון
+    int[] rdc = new int[10]
+
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write("how many familiesin town?");
+        int Families = int.Parse(Console.ReadLine());
+
+        int Total = CalculateTotalDiscount(Families);
+
+        if (Total > rdc[i])
+        {
+            Console.WriteLine("town " + i + "exceeded the limit.);
+}
+
+
